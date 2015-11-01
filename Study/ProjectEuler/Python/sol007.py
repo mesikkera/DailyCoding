d7fb9c69de22515728b65c3d1974829a12c19238
mesikkera <mesikkera@gmail.com>
2015-11-02 00:39:56 +0900

    Project euler sol 007 with py.

--- Study/ProjectEuler/Python/sol007.py
+++ Study/ProjectEuler/Python/sol007.py
@@ -15,30 +15,30 @@ What is the 10 001st prime number?
 10001번째 소수는 무엇인가?
 '''
 import time
-
-# def is_prime(natural_number):
-# 	# prime number: 1과 자기자신외에는 나누어어지지 않는 수
-# 	is_prime_number = False
-# 	for number in range(1, natural_number, 1):
-# 		if natural_number % number != 0 and natural_number % natural_number == 0:
-# 			is_prime_number == True
-# 	return is_prime_number
-
-
-def prime_number(natural_number):
-	prime_number_list = []
-	number = 1
-	while True:
-		if 4natural_number % number != 0:
-			prime_number_list.append(number)
-
-		if natural_number == number:
-			break
-		number += 1
-	print prime_number_list
-
+ 
+def is_prime(n):
+    if n % 2 == 0: 
+    	return False
+    p = 3
+    while p < n**0.5+1:
+        if n % p == 0: 
+        	return False
+        p += 2
+    return True
+ 
+def find_nth_prime(n):
+    prime_number = 2
+    count = 1
+    iter = 3
+    while count < n:
+        if is_prime(iter):
+            prime_number = iter
+            count += 1
+        iter += 2
+    return prime_number
+ 
 start = time.time()
-product = prime_number(100)
-elapsed = time.time() - start
-
-print "result %s returned in %s seconds." % (product,elapsed)
+prime = find_nth_prime(10001)
+elapsed = (time.time() - start)
+ 
+print "found %s in %s seconds" % (prime,elapsed)
