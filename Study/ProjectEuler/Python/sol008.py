e8ef1d6227f7472e25c5bf374279444610577552
mesikkera <mesikkera@gmail.com>
2015-11-03 21:15:23 +0900

    modify project euler sol 008 with py and  add project euler 009 with py

--- Study/ProjectEuler/Python/sol008.py
+++ Study/ProjectEuler/Python/sol008.py
@@ -26,18 +26,38 @@ The four adjacent digits in the 1000-digit number that have the greatest product
 71636269561882670428252483600823257530420752963450
 
 Find the thirteen adjacent digits in the 1000-digit number that have the greatest product. What is the value of this product?
-
-연속된 1000자리 숫자 
 '''
-
 import time
+import operator
+ 
+def find_greatest_product():
+	index = 0
+	greatest_product = 0
+	number = 7316717653133062491922511967442657474235534919493496983520312774506326239578318016984801869478851843858615607891129494954595017379583319528532088055111254069874715852386305071569329096329522744304355766896648950445244523161731856403098711121722383113622298934233803081353362766142828064444866452387493035890729629049156044077239071381051585930796086670172427121883998797908792274921901699720888093776657273330010533678812202354218097512545405947522435258490771167055601360483958644670632441572215539753697817977846174064955149290862569321978468622482839722413756570560574902614079729686524145351004748216637048440319989000889524345065854122758866688116427171479924442928230863465674813919123162824586178664583591245665294765456828489128831426076900422421902267105562632111110937054421750694165896040807198403850962455444362981230987879927244284909188845801561660979191338754992005240636899125607176060588611646710940507754100225698315520005593572972571636269561882670428252483600823257530420752963450
+	 
+	digit_list = [int(x) for x in str(number)]
+	digit_list_length = len(digit_list) # 1000
 
-def find_biggest_number():
-	pass
+	while index < digit_list_length:
+		if index == 0:
+		  temp_product = reduce(operator.mul, digit_list[index: index +5])
+		elif index == 1:
+		  temp_product = reduce(operator.mul, digit_list[index - 1: index + 4])
+		elif index == digit_list_length - 1:
+		  temp_product = reduce(operator.mul, digit_list[index - 4: index + 1])
+		elif index == digit_list_length - 2:
+		  temp_product = reduce(operator.mul, digit_list[index - 3: index + 2])
+		else:
+		  temp_product = reduce(operator.mul, digit_list[index -2 : index + 3])
+
+		if temp_product > greatest_product: 
+			greatest_product = temp_product
+		index += 1
+	return greatest_product
 
 start = time.time()
-biggest_number = find_biggest_number()
+greatest_product = find_greatest_product()
 elapsed = (time.time() - start)
 
-print "found %s in %s seconds" % (biggest_number,elapsed)
+print "found %s in %s seconds" % (greatest_product,elapsed)
 
