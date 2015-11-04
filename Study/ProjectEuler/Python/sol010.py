d22f29b09f75a87919e4b3968ccf513144cb7250
mesikkera <mesikkera@gmail.com>
2015-11-04 19:43:50 +0900

    project euler sol 010 with py

--- Study/ProjectEuler/Python/sol010.py
+++ Study/ProjectEuler/Python/sol010.py
@@ -7,26 +7,52 @@ Summation of primes
 The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
 
 Find the sum of all the primes below two million.
-
-10 이하의 소수를 모두 더하면 2 + 3 + 5 + 7 = 17 이 됩니다.
-
-이백만(2,000,000) 이하 소수의 합은 얼마입니까?
 '''
 import time
+import math
 
-def is_prime(number): 
-	if number % 2 == 0:  
-		return False 
-	odd_number = 3
-	while odd_number < number**0.5+1:
-		if number % odd_number == 0: 
-			return False
-    	odd_number += 2 
-    	return True
+# def is_prime(number):
+#     oldnum = number
+#     factor = 1
+#     while number > 1:
+#         factor += 1
+#         if number % factor == 0:
+#             if 1 < factor < oldnum:
+#                 return False # is not prime
+#             number //= factor
+#     return True # is prime!
+
+# def is_prime(n):
+#     for i in range(3, n):
+#         if n % i == 0:
+#             return False
+#     return True
+
+# def is_prime(n):
+#     for x in xrange(3, int(n**0.5)+1):
+#         if n % x == 0:
+#             return False
+#     return True    
+
+# def eratosthenes(n):
+#      multiples = set()
+#      for i in range(2, n+1):
+#         if i not in multiples:
+#             yield i
+#             multiples.update(range(i*i, n+1, i))
+#   	print(list(eratosthenes(2000000)))
+
+def is_prime(number):
+	for factor in xrange(3, int(number**0.5) + 1, 2):
+		if number % factor == 0:
+			return False 
+	return True
 
 def find_summation_of_primes(end_number):
-	summation_of_primes = 0
-	for number in range(1, end_number+1, 1):
+	summation_of_primes = 2
+
+	# 홀수만 검사
+	for number in range(3, end_number+1, 2):
 		if is_prime(number):
 			summation_of_primes += number
 	return summation_of_primes
