09c5a252529a39049928e3eb3aa0b02f826e2d41
mesikkera <mesikkera@gmail.com>
2015-11-05 02:28:57 +0900

    Add Template 4

---
+++ Study/ModernC++Programming/Chapter11/myAddTemplate4.cpp/myAddTemplate4.cpp/myAddTemplate4.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+template <typename T>
+T const add(T const& a, T const& b)
+{
+	cout << "typename T" << endl;
+	return a + b;
+}
+
+template <>
+char* const add<char*>(char* const& a, char* const& b)
+{
+	cout << "specialized" << endl;
+	return strcat(a, b);
+}
+
+int main()
+{
+	int i = 5;
+	int j = 10;
+
+	double a = 5.1;
+	double b = 10.2;
+
+	char m[20] = "Hello";
+	char n[20] = "World";
+
+	cout << i << " + " << j << " = " << add(i, j) << endl;
+	cout << a << " + " << b << " = " << add(a, b) << endl;
+	cout << add<char*>(m, n) << endl;
+
+	return 0;
+}
