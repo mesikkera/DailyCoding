35aa78ec1a2eb33008c03fd3e3f687bb70a61a69
mesikkera <mesikkera@gmail.com>
2015-11-27 23:00:29 +0900

    stack

---
+++ Study/ModernC++Programming/Chapter12/stack/stack/stack1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+int main()
+{
+	stack<int> st;
+
+	st.push(10);
+	st.push(20);
+	st.push(30);
+	st.push(40);
+
+	while (!st.empty()) {
+		cout << st.top() << " ";
+		st.pop();
+	}
+
+	return 0;
+}
