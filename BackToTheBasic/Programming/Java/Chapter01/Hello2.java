287c494d35432071d35e476fd62c1ddedce49887
mesikkera <mesikkera@gmail.com>
2015-12-02 01:57:25 +0900

    Java comment example

---
+++ BackToTheBasic/Programming/Java/Chapter01/Hello2.java
@@ -0,0 +1,10 @@
+/*
+Date: 2015. 12. 02
+Source: Hello2.java
+Author: mesikkera
+*/
+class Hello2 {
+	public static void main(String[] args) {
+		System.out.println("Hello!");	// print "Hello"
+	}
+}
