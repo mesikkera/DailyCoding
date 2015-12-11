9ba8d17289df34b7bdc73f464ed24019c4ea22a5
mesikkera <mesikkera@gmail.com>
2015-12-12 00:14:52 +0900

    math const

---
+++ BackToTheBasic/Programming/Java200/MathConstMain.java
@@ -0,0 +1,10 @@
+public class MathConstMain {
+	public static void main(String[] args) {
+		System.out.println(Math.PI);
+		System.out.println(Math.E);
+		// 원의 넓이
+		double r = 10;
+		double s = Math.PI * r * r;
+		System.out.println("원의 넓이" + s);
+	}
+}
