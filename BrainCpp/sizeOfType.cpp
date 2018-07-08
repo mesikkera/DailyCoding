7e692d18f3bc16a5d8ac5d5faaaee071029eea32
mesikkera <mesikkera@gmail.com>
2018-07-08 16:32:54 +0900

    start Brain Cpp

---
+++ BrainCpp/sizeOfType.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "short int: " << sizeof(short int) << " bytes\n";
+    cout << "unsigned short int: " << sizeof(unsigned short int) << " bytes\n";
+    cout << "int: " << sizeof(int) << " bytes\n";
+    cout << "unsigned int: " << sizeof(unsigned int) << " bytes\n";
+    cout << "long int: " << sizeof(long int) << " bytes\n";
+    cout << "unsigned long int: " << sizeof(unsigned long int) << " bytes\n";
+    return 0;
+}
---
+++ BrainCpp/usingNotation.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int decimal = 41;
+    int octal = 041;
+    int hexadecimal = 0x41;
+
+    cout << "41 = " << decimal << endl;
+    cout << "041 = " << octal << endl;
+    cout << "0x41 = " << hexadecimal << endl;
+
+    return 0;
+}
