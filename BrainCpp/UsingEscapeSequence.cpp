9fc6f7587616a557293b28e86380f7a6c2644a55
mesikkera <mesikkera@gmail.com>
2018-07-08 18:17:01 +0900

    이스케이프 시퀀스 사용해보기

---
+++ BrainCpp/UsingEscapeSequence.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // Hello, World!를 쓰려다 실수했음.
+    cout << "Hello, Works";
+
+    // 백스페이스로 뒤에 두 글자를 지운다.
+    cout << "\b\b";
+
+    // Hello, World!를 마무리한다.
+    cout << "ld!";
+
+    return 0;
+}
