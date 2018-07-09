68171cb2c2c54956c601c57b7eef6c831b5fbded
mesikkera <mesikkera@gmail.com>
2018-07-09 14:53:59 +0900

    009.  순환문 배우기

---
+++ Cpp200/009_LearningLoopStatement.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int sum1 = 0;
+    int sum2 = 0;
+
+    int one = 1;
+    int two = 2;
+
+    for (int i = 0; i < 5; i++)
+    {
+        sum1 += one;
+        sum2 += two;
+    }
+
+    cout << "Sum Result: " << sum1 << ", " << sum2 << endl;
+    return 0;
+}
