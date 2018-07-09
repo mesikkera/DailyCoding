f0c31eed3c99ba35a7d57387db34351d556c0d10
mesikkera <mesikkera@gmail.com>
2018-07-09 11:00:38 +0900

    상수형 변수 관련 내용 추가

--- Cpp200/004_LearningConst.cpp
+++ Cpp200/004_LearningConst.cpp
@@ -3,6 +3,8 @@ using namespace std;
 
 int main()
 {
+    // 상수형 변수는 일반 변수와 달리 한 번 값이 
+    // 할당되면 프로그램이 종료될 때까지 값이 변할 수 없다.
     const int GREATE_VICTORY_SALSU = 612;
     const int GREATE_VICTORY_GWIJU = 1019;
 
