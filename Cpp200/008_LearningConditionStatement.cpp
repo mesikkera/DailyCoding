cf1a27d1bcbca31b40dff1c6d4fbab350b1cf076
mesikkera <mesikkera@gmail.com>
2018-07-09 14:51:13 +0900

    008. 조건문 배우기

---
+++ Cpp200/008_LearningConditionStatement.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x = 10;
+    int y = 1;
+
+    if ( x > y )
+        cout << "x is bigger than y" << endl;
+    else
+        cout << "x is smaller than y" << endl;
+
+    return 0;
+}
