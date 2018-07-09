7e54dee277d51c1dd128ab1ddf2c91b46f714893
mesikkera <mesikkera@gmail.com>
2018-07-09 15:58:27 +0900

    009. 주석 배우기

---
+++ Cpp200/012_LearningComments.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // 주석의 용도
+    // 01. 현재 실행되면 안 되는 부분을 가리키는 것.
+    // 02. 소스 코드를 이해하는 데 필요한 설명 글 붙이기
+    //출력하지 않을 코드
+    // cout << " TEST " << endl;
+
+    // 0부터 9까지 출력
+    /* for (int i = 0; i < 10; i++)
+            cout << i << ", " */
+    return 0;
+}
