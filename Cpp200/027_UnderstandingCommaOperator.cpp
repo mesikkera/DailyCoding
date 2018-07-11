2072fccbfdf9deab893fe2c51ebd5e69bbb8a479
mesikkera <mesikkera@gmail.com>
2018-07-11 14:59:08 +0900

    027. 쉼표 연산자 이해하기

---
+++ Cpp200/027_UnderstandingCommaOperator.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+//using namespace std;
+
+int main()
+{
+    int goguryeo = 37, baekjae = 18, silla = 57;
+
+    printf("Founding year of Three Kingdom\n");
+    printf("Goguryeo bc%d, Baekjae bc%d, Silla bc%d\n", goguryeo, baekjae, silla);
+
+    return 0;
+}
