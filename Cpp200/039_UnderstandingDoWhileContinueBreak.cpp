6d9c047c640c2e3c9f13b15f2f0ed150b2e90370
mesikkera <mesikkera@gmail.com>
2018-07-13 11:13:48 +0900

    039. 조건 순환문 이해하기 (do ~ while ~ continue ~ break)

---
+++ Cpp200/039_UnderstandingDoWhileContinueBreak.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int number = 0;
+
+    do {
+        number++;
+
+        if (number % 3 == 0)
+            continue;
+        else
+            cout << "number value : " << number << endl;
+    } while (number < 10);
+
+    return 0;
+}
