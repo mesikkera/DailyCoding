27e502752265ea4de0953f9d6d1d3f4a5ad67569
mesikkera <mesikkera@gmail.com>
2018-07-13 15:08:38 +0900

    045. 절대값과 제곱수 구하기 (abs, fabs, pow)

---
+++ Cpp200/045_AbsFabsPow.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    cout << "== Find the absolute value == " << endl;
+    cout << "abs(-10)   : " << abs(-10) << endl;
+    cout << "abs(-5.72) : " << fabs(-5.72) << endl;
+
+    cout << "== Find the square power == " << endl;
+    cout << "pow(2, 2) : " << pow(2, 2) << endl;
+    cout << "pow(3, 4) : " << pow(3, 4) << endl;
+    cout << "pow(4, 6) : " << pow(4, 6) << endl;
+    cout << "pow(8, 3) : " << pow(8, 3) << endl;
+
+    return 0;
+}
