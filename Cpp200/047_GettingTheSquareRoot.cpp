22bb4a2b3faa121126b89cdb6867980806c70e34
mesikkera <mesikkera@gmail.com>
2018-07-13 17:13:57 +0900

    047. 제곱근 구하기 (sqrt)

---
+++ Cpp200/047_GettingTheSquareRoot.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+// #include <math.h>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    cout << "Square Root of 4 : " << sqrt(4.0) << endl;
+    cout << "Square Root of 16: " << sqrt(16) << endl;
+    cout << "Square Root of 64: " << sqrt(64) << endl;
+    cout << "Square Root of 256: " << sqrt(256) << endl;
+    
+    return 0;
+}
