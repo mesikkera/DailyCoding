ed51a096471584086f7f9e2afd97b41ee3b60736
mesikkera <mesikkera@gmail.com>
2018-07-15 12:26:52 +0900

    055. 문자열 길이 구하기 (length)

---
+++ Cpp200/055_StringLength.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string test_string1 = "South Korea";
+    string test_string2 = "Republic of Korea";
+
+    cout << "Test String1 : " << test_string1.length() << endl;
+    cout << "Test String2 : " << test_string2.length() << endl;
+
+    return 0;
+}
