1e8b496a6e76311f8a92f93a41cbee90d61af023
mesikkera <mesikkera@gmail.com>
2018-07-17 13:02:51 +0900

    074. Call by Value 이해하기

--- Cpp200/074_CallByValue.cpp
+++ Cpp200/074_CallByValue.cpp
@@ -12,7 +12,11 @@ void Func(int arg)
 int main()
 {
     int year = 10;
-
+    
+    // Call by Value는 인자로 넘어온 값을 내부적으로 복사해서 사용.
+    // year 변수에 직접 10을 더하는 것이 아니라 
+    // 내부적으로 복사한 값에 10을 더한 셈.
+    // 따라서 year 변수의 값은 변함이 없다.
     Func(year);
 
     cout << "After Call By Value : " << year << endl;
