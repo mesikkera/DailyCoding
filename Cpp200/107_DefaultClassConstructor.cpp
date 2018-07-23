c2a938c903a8848b363934c5266046b5dad8c962
mesikkera <mesikkera@gmail.com>
2018-07-23 14:39:06 +0900

    107. default 클래스 생성자

---
+++ Cpp200/107_DefaultClassConstructor.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Class1 
+{
+public:
+    class1() = default;
+
+public:
+    int number;
+    double prime;
+    string word;   
+};
+
+class Class2 
+{
+
+}
