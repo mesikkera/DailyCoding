b8834aa73d089d8a8154e0857e44f99602371bd3
mesikkera <mesikkera@gmail.com>
2018-07-23 16:32:36 +0900

    ++ 연산자 오버로딩 완성

--- BrainSTL/operator++.cpp
+++ BrainSTL/operator++.cpp
@@ -8,13 +8,27 @@ class Point
 public:
     Point(int _x = 0, int _y = 0):x(_x), y(_y) {}
     void Print() const { cout << x << ", " << y << endl; }
-    void operator++ ()
+    // void operator++ ()
+    // {
+    //     // 전위 ++ 연산자
+    // }   
+    // void operator++(int)
+    // {
+    //     // 후위 ++ 연산자
+    // }
+    const Point& operator++() // 전위++
     {
-        // 전위 ++ 연산자
-    }   
-    void operator++(int)
+        ++x;
+        ++y;
+        return *this;
+    }
+
+    const Point operator++(int) // 후위++
     {
-        // 후위 ++ 연산자
+        Point pt(x, y);
+        x++;
+        y++;
+        return pt;
     }
 };
 
@@ -22,9 +36,21 @@ int main()
 {
     Point p1(2, 3), p2(2, 3);
 
-    ++p1; // p1.operator++(); 
+    // ++p1; // p1.operator++(); 
 
-    p2++; // p2.oerator++(0);
+    // p2++; // p2.oerator++(0);
+
+    Point result;
+
+    result = ++p1;      // p1.operator++();
+    p1.Print();
+    result.Print();
+
+    result = p2++;      // p2.operator++(0);
+    p2.Print();
+    result.Print();
+
+    return 0;
 
     return 0;
 }
