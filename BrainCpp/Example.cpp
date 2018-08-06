f9068bb3b2afee6bd95ec5c76d09068ca2f46764
mesikkera <mesikkera@gmail.com>
2018-08-06 15:49:19 +0900

    두 점 사이의 거리 구하기(파일 분할1)

---
+++ BrainCpp/Example.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+struct Point 
+{
+    int x, y;
+};
+
+// 두 점의 거리를 구하는 함수의 원형
+double Distance(Point p1, Point p2);
+
+int main()
+{
+    // 두 점
+    Point a = {0, 0};
+    Point b = {3, 4};
+
+    // 두 점의 거리
+    double dist_a_b = Distance(a, b);
+
+    // 결과 출력
+    cout << "( " << a.x << ", " << a.y << " ) 와";
+    cout << " ( " << b.x << ", " << b.y << " ) 의 거리 = " << dist_a_b << endl;
+
+    return 0;
+}
+
+double Distance(Point p1, Point p2)
+{
+    // 피타고라스 정의 
+    double distance;
+    distance = sqrt( pow(p1.x - p2.x, 2) + pow(p1.y - p2.y, 2) );
+
+    // 결과 반환
+    return distance;
+}
