b2a1cbbec92df340f741e43c96900b8c04933831
mesikkera <mesikkera@gmail.com>
2018-08-06 15:52:14 +0900

    두 점의 거리 함수 원형 분리 및 헤더파일 인클루드

--- BrainCpp/Example.cpp
+++ BrainCpp/Example.cpp
@@ -2,13 +2,14 @@
 #include <cmath>
 using namespace std;
 
-struct Point 
-{
-    int x, y;
-};
+#include "Example.h"
+// struct Point 
+// {
+//     int x, y;
+// };
 
-// 두 점의 거리를 구하는 함수의 원형
-double Distance(Point p1, Point p2);
+// // 두 점의 거리를 구하는 함수의 원형
+// double Distance(Point p1, Point p2);
 
 int main()
 {
