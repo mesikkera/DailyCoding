e612cad68d9311b2279ea649f330b7b67f9c0e30
mesikkera <mesikkera@gmail.com>
2018-08-08 09:44:31 +0900

    객체의 생성과 사용

--- BrainCpp/PointClass.cpp
+++ BrainCpp/PointClass.cpp
@@ -12,12 +12,27 @@ class Point
 		// 멤버 함수
 		void Print()
 		{
+			// 멤버 변수에서 사용하고 있는 X, Y는 
+			// 자기 자신의 멤버 변수를 의미
 			cout << " ( " << x << ", " << y << " )\n";
 		}
 };
 
 int main()
 {
+	// 객체 생성
+	Point pt1, pt2;
+
+	// 객체 초기화
+	pt1.x = 100;
+	pt1.y = 100;
+	pt2.x = 200;
+	pt2.y = 200;
+
+	// 멤버 함수 호출
+	pt1.Print();
+	pt2.Print();
+	
 	return 0;
 }
 
