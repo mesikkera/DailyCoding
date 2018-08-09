745904c222f6a4d18984a8fea95459d27b80d44f
mesikkera <mesikkera@gmail.com>
2018-08-09 15:49:01 +0900

    멤버 함수에 대한 포인터2 (Point::Print())

--- BrainCpp/MemberFunctionPointer.cpp
+++ BrainCpp/MemberFunctionPointer.cpp
@@ -8,6 +8,9 @@ typedef void (*FP1)(int);
 // Point 클래스의 멤버 함수에 대한 포인터
 typedef void (Point::*FP2)(int);
 
+// Point 클래스의 멤버 함수에 대한 포인터: Point::Print()
+typedef void (Point::*FP3)();
+
 int main()
 {
     // 객체 생성
@@ -16,12 +19,14 @@ int main()
     // FP1, FP2를 사용해서 SetX() 함수를 가리킨다.
     // FP1 fp1 = &Point::SetX;              // 오류
     FP2 fp2 = &Point::SetX;                 // 성공: SetX() 함수의 주소를 fp2에 대입한다.
+    FP3 fp3 = &Point::Print;
 
     // 함수 포인터를 사용해서 함수 호출
     (pt.*fp2)(100);
 
     // 내용 출력
     pt.Print();
-
+    (pt.*fp3)();
+    
     return 0;
 }
