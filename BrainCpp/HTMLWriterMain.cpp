93083399b30eaa94bf4432e01a3d072c5ad16846
mesikkera <mesikkera@gmail.com>
2018-08-11 11:57:23 +0900

    부모 클래스의 포인터를 이용한 함수 호출

--- BrainCpp/HTMLWriterMain.cpp
+++ BrainCpp/HTMLWriterMain.cpp
@@ -8,6 +8,13 @@ int main()
     // hw.SetFont("D2Coding", 20, "Red");
     // hw.Write();
     HTMLWriter hw("test.html", "Auto Generated HTML Document");
-    hw.Write();
+
+    // 부모 클래스의 포인터로 가리킨다.
+    DocWriter* pdw = &hw;
+
+    //hw.Write();
+    //  포인터를 사용해서 저장함수 호출
+    pdw->Write();
+    
     return 0;
 }
