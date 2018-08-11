9cce8511926bae1a183a091470f67265137b18ad
mesikkera <mesikkera@gmail.com>
2018-08-11 16:03:44 +0900

    부모 클래스에서 오버로드된 함수 중에 어느 것 하나라도 오버라이드하면 나머지 다른 함수들도 모두 사용할 수 없다.

--- BrainCpp/OverloadedMemberFunction.cpp
+++ BrainCpp/OverloadedMemberFunction.cpp
@@ -28,7 +28,17 @@ class Dog : public Pet
 {
     // 애완 동물 클래스를 상속받아 강아지 클래스 정의 
     // 현재는 아무런 멤버도 없다.
+
+    // 인자가 없는 Eat() 함수를 오버라이딩
+public:
+    void Eat();
+
 };
+
+void Dog::Eat()
+{
+    cout << name << " says, 'Growl~'\n";
+}
 
 int main()
 {
