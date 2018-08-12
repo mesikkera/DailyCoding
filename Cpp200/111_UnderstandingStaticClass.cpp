f53933cd09cdbdf9bdfa1149095db78bb993b14a
mesikkera <mesikkera@gmail.com>
2018-08-12 16:29:46 +0900

    static 클래스 이해하기(statc)

---
+++ Cpp200/111_UnderstandingStaticClass.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+static class Calculator
+{
+public:
+    int Plus(const int x, const int y) const 
+    {
+        return x + y;
+    }
+} calc;
+
+int main()
+{
+    cout << "1 + 2 = " << calc.Plus(1, 2) << endl;
+
+    return 0;
+}
