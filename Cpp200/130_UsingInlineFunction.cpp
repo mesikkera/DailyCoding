0ed7d7a2c5543e5b0c2ce3e7b7196ac90ede0b4c
mesikkera <mesikkera@gmail.com>
2018-08-13 18:22:07 +0900

    130. 인라인 함수 사용하기(inline)

---
+++ Cpp200/130_UsingInlineFunction.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+inline int Max(int x, int y)
+{
+    return x > y ? x : y;
+}
+
+int main()
+{
+    for (int i = 0; i < 5; i++)
+        cout << Max(i, i + 10) << endl;
+    return 0;
+}
