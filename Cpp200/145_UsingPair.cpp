5947642ae83327f95a2508c19e946e7058153d11
mesikkera <mesikkera@gmail.com>
2018-08-15 14:29:33 +0900

    145. pair 사용하기

---
+++ Cpp200/145_UsingPair.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    pair<string, int> data("Ronaldo", 7);
+
+    cout << data.first << " " << data.second << endl;
+
+    return 0;
+}
