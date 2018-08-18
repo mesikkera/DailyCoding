82550ea94a7c826ce6bb79565540e813b1483927
mesikkera <mesikkera@gmail.com>
2018-08-18 12:17:42 +0900

    156. list 삽입, 정렬, 중복 값 제거하기, 합치기(sort, unique, merge)

---
+++ Cpp200/156_ListSortUniqueMerge.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <list>
+
+using namespace std;
+
+void Print(const list<int> &data)
+{
+    cout << "== Print ==" << endl;
+
+    for (auto iter = data.begin(); iter != data.end(); ++iter)
+        cout << *iter << endl;
+
+    cout << endl;
+}
+
+int main()
+{
+    list<int> data1({1, 2, 3,2});
+    data1.push_back(3);
+    data1.sort();
+    
+    Print(data1);
+
+    data1.unique();
+
+    Print(data1);
+
+    list<int> data2 = {4, 5, 6};
+    data2.merge(data1);
+
+    Print(data2);
+    return 0;
+}
