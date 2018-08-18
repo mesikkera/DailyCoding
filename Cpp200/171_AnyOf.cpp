2886010a6437017bfd7c2982d4b020e3f48323f5
mesikkera <mesikkera@gmail.com>
2018-08-18 21:11:57 +0900

    171. 조건에 맞는 요소가 있는지 검사하기(any_of)

--- Cpp200/171_AnyOf.cpp
+++ Cpp200/171_AnyOf.cpp
@@ -14,7 +14,7 @@ int main()
 {
     vector<int> data = {8, 27, 35, 49};
 
-    // any_of(): 컨테이너 요소 중 하나라도 조건에 맞다면 true 리턴
+    // any_of(): 컨테이너 요소 중 하나라도 조건에 맞다면 true 리턴  
     if(any_of(data.begin(), data.end(), EvenOdd))
         cout << "data : 짝수가 존재합니다." << endl;
 
