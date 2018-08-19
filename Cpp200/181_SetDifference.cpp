0476baa25cf31ff6ebc0da37afd7a6984d2aa5a9
mesikkera <mesikkera@gmail.com>
2018-08-19 15:55:25 +0900

    181. 두 영역의 차집합 구하기(set_difference)

--- Cpp200/181_SetDifference.cpp
+++ Cpp200/181_SetDifference.cpp
@@ -12,6 +12,7 @@ int main()
     vector<int> diff1;
     vector<int> diff2;
 
+    // set_dirrerence(): 2개의 컨테이너의 내용을 비교하여 새로운 컨테이너에 그 차이를 저장. 
     set_difference(data1.begin(), data1.end(), data2.begin(), data2.end(), inserter(diff1, diff1.begin()));
     set_difference(data1.begin() + 2, data1.end(), data2.begin() + 2, data2.end(), inserter(diff2, diff2.begin()));
 
