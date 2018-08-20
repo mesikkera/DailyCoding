a7e2cc3bd7eb80275fc6e916b979a21b8c5c6099
mesikkera <mesikkera@gmail.com>
2018-08-20 16:16:01 +0900

    193. 시작, 끝 문자로 문자열 찾기(regex_match)

---
+++ Cpp200/193_RegexMatch.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <regex>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    vector<string> Korea = {
+        "Republic of Korea", 
+        "republic of Korea, since 1945",
+        "Republic of korea, since 1945",
+        "republic of korea",
+        "south Korea, since 1945",
+        "south korea", 
+        "SOUTH KOREA, since 1945",
+        "south Korea",
+    };
+
+    regex re("(SOUTH|south).*(1945)");
+
+    smatch match_info;
+
+    for (auto i : Korea)
+    {
+        if (regex_match(i, match_info, re))
+            cout << match_info[0] << endl;
+    }
+    return 0;
+}
