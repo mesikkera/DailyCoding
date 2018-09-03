4017bab3101f857ab045a28af57a8b994a77e9dc
mesikkera <mesikkera@gmail.com>
2018-09-03 13:11:07 +0900

    Point 객체를 출력하지 못하는 함수 템플릿 예제

---
+++ BrainSTL/FunctionTemplateSpecialization.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+class Point 
+{
+    int x; int y;
+public:
+    explicit Point(int _x = 0, int _y = 0): x(_x), y(_y) { }
+    void Print() const { cout << x << ", " << y << endl; }
+};
+
+template<typename T>
+void Print(T a)
+{
+    cout << a << endl;
+}
+
+int main()
+{
+    int n = 10;
+    double d = 2.5;
+    Point pt(2, 3);
+
+    Print(n);
+    Print(d);
+    // Print(pt);      // 에러! cout << pt 연산이 불가능하다.
+
+    return 0;
+}
