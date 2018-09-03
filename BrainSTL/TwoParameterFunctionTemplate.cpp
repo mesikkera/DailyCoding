b53d29447eabf974481a1bd896bf80573bca5959
mesikkera <mesikkera@gmail.com>
2018-09-03 12:17:28 +0900

    두개의 매개변수를 갖는 함수 템플릿

---
+++ BrainSTL/TwoParameterFunctionTemplate.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+template<typename T1, typename T2>
+void Print(T1 a, T2 b)
+{
+    cout << a << ", " << b << endl;
+}
+
+int main()
+{
+    Print(10, 10.5);                // 정수, 실수 출력
+    Print("Hello!", 100);           // 문자열, 정수 출력
+    Print(1.5, "Hello!");           // 실수, 문자열 출력
+
+    return 0;
+}
