4c2a147884ad7e3487b21624096431f94a88f9c3
mesikkera <mesikkera@gmail.com>
2018-09-03 10:57:56 +0900

    정수, 실수, 문자열 출력 오버로딩 함수

--- BrainSTL/TwoIntegerPrint.cpp
+++ BrainSTL/TwoIntegerPrint.cpp
@@ -1,12 +1,27 @@
 #include <iostream>
 using namespace std;
 
+// 정수 출력
 void Print(int a, int b)
+{
+    cout << a << ", " << b << endl;
+}
+
+void Print(double a, double b)
+{
+    cout << a << ", " << b << endl;
+}
+
+void Print(const char* a, const char* b)
 {
     cout << a << ", " << b << endl;
 }
 
 int main()
 {
-    Print(10, 20);
+    Print(10, 20);          // 정수 출력
+    Print(0.123, 1.123);    // 실수 출력
+    Print("ABC", "abcde");  // 문자열 출력
+    return 0;
 }
+
