6befb9b655187e2344961645a16e6611454a8042
mesikkera <mesikkera@gmail.com>
2018-09-04 17:49:36 +0900

    find 알고리즘

---
+++ BrainSTL/FindAlgorithm.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>        // find 사용
+using namespace std;
+
+int main()
+{
+    vector<int> v;
+    v.push_back(10);
+    v.push_back(20);
+    v.push_back(30);
+    v.push_back(40);
+    v.push_back(50);
+
+    vector<int>::iterator iter;
+    iter = find(v.begin(), v.end(), 20);           // [begin, end)에서 20 찾기
+    cout << *iter << endl;
+
+    iter = find(v.begin(), v.end(), 100);
+    if(iter == v.end())     // 100이 없으면 iter == v.end()임
+        cout << "100 is not found!" << endl;
+
+    return 0;
+}
