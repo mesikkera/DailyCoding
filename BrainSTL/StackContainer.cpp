c6d90ee479a4430a4ab78a2e91a171c30aa0f858
mesikkera <mesikkera@gmail.com>
2018-09-04 18:49:59 +0900

    stach 컨테이너

---
+++ BrainSTL/StackContainer.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+int main()
+{
+    stack<int> st;              // stack 컨테이너 생성
+
+    st.push(10);                // 데이터 추가(입력)
+    st.push(20);    
+    st.push(30);
+    
+    cout << st.top() << endl;   // top 데이터 출력
+    st.pop();                   // top 데이터 삭제
+
+    cout << st.top() << endl;   
+    st.pop();
+
+    cout << st.top() << endl;
+    st.pop();
+
+    if(st.empty())              // 스택이 비었는지 확인
+        cout << "There is no data in stack." << endl;
+
+    return 0;
+}
