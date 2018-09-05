3c06cf40b3dc728194bb2dc1733a01929202c530
mesikkera <mesikkera@gmail.com>
2018-09-05 12:44:21 +0900

    vector의 push_back()

---
+++ BrainSTL/VectorPushBack.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    vector<int> v;
+
+    v.push_back(10);
+    v.push_back(20);
+    v.push_back(30);
+    v.push_back(40);
+    v.push_back(50);
+
+    // i --> int type
+    // v.size() --> unsigned int type 반환
+    for (int i = 0; i < v.size(); ++i)
+        cout << v[i] << endl;
+
+    return 0;
+}
