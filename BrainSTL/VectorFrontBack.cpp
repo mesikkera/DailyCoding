ea8d6abfc5230362af438b768995359fa1648423
mesikkera <mesikkera@gmail.com>
2018-09-06 19:51:03 +0900

    일반 반복자와 상수 반복자

--- BrainSTL/VectorFrontBack.cpp
+++ BrainSTL/VectorFrontBack.cpp
@@ -50,10 +50,41 @@ int main()
         cout << v[i] << " ";
     cout << endl;
 
+    v.clear();
+    v.push_back(10);
+    v.push_back(20);
+    v.push_back(30);
+    v.push_back(40);
+    v.push_back(50);
+
     // begin(), end()
     for(vector<int>::iterator iter = v.begin(); iter != v.end(); ++iter)
         cout << *iter << " ";
     cout << endl;
+
+    vector<int>::iterator iter = v.begin();     // 시작 원소를 가리킨다.
+    cout << *iter << endl;
+
+    iter += 2;      // +2한 위치의 원소를 가리킨다. 
+    cout << *iter << endl;
+
+    iter -= 1;      // -1한 위치의 원소(20)를 가리킨다.
+    cout << *iter << endl;
+
+    // 이미 선언되어 있으므로 생략
+    // vector<int>::iterator iter = v.begin();
+    vector<int>::const_iterator citer = v.begin();
+
+    cout << *iter << endl;      // 가리키는 원소의 참조
+    cout << *citer << endl;     // 가리키는 원소의 상수 참조
+
+    cout << *iter << endl;      // 다음 원소로 이동한 원소의 참조
+    cout << *citer << endl;     // 다음 원소로 이동한 원소의 상수 참조
+
+    *iter = 100;                // 일반 반복자는 가리키는 원소를 변경할 수 있음
+    // *citer = 100;            // 상수 반복자는 가리키는 원소를 변경할 수 없음.
+
+
 
     return 0;
 }
