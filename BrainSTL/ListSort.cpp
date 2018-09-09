fdaa61a5e747b1c3e987b317a3550d62ee684c69
mesikkera <mesikkera@gmail.com>
2018-09-09 12:16:09 +0900

    list의 sort()

--- BrainSTL/ListSort.cpp
+++ BrainSTL/ListSort.cpp
@@ -15,7 +15,11 @@ int main()
     for(iter = lt.begin(); iter != lt.end(); ++iter)
         cout << *iter << " ";
     cout << endl;
-    
+
+    // sort()
+    // 정렬의 기준은 오름차순으로 < 연산한 결과.
+    // 비교 대상인 두 원소를 < 연산하여 정렬을 수행
+    // < 연산자는 less 조건자의 연산 결과와 같다.
     lt.sort();      // 오름차순 정렬
 
     for(iter = lt.begin(); iter != lt.end(); ++iter)
