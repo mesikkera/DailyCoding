49a68d7b16452ec0a7331698b26f87fc4e92f59c
mesikkera <mesikkera@gmail.com>
2018-09-11 12:50:39 +0900

    set의 정렬 기준을 사용한 값 비교

--- BrainSTL/Set.cpp
+++ BrainSTL/Set.cpp
@@ -22,12 +22,19 @@ int main()
     cout << "원소 50의 개수 : " << s.count(50) << endl;
     cout << "원소 100의 개수 : " << s.count(100) << endl;
 
-    // set의 find()
+// set의 find()
     iter = s.find(30);      // 30의 반복자를 반환
     if(iter != s.end())
         cout << *iter << "이 s에 있다." << endl;
     else 
         cout << "s에 30이 없다" << endl;
+
+// set의 정렬 기준을 사용한 값 비교
+
+    // 30과 50의 비교
+    cout << (!s.key_comp()(30, 50) && !s.key_comp()(50, 30)) << endl;
+    // 30과 30의 비교
+    cout << (!s.key_comp()(30, 30) && !s.key_comp()(30, 30)) << endl;
 
     return 0;
 }
