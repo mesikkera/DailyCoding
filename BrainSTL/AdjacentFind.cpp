e7c43d4a784b49da3a6863ffefb1682cfbcd4fab
mesikkera <mesikkera@gmail.com>
2018-09-14 14:14:42 +0900

    adjacent_find() 알고리즘의 반환 반복자

--- BrainSTL/AdjacentFind.cpp
+++ BrainSTL/AdjacentFind.cpp
@@ -29,5 +29,25 @@ int main()
         cout << *iter << " ";
     cout << endl;
 
+// adjacent_fint() 알고리즘의 반환 반복자
+    vector<int> v1;
+    v1.push_back(10);
+    v1.push_back(20);
+    v1.push_back(30);
+    v1.push_back(40);
+    v1.push_back(50);
+
+    for(vector<int>::size_type i = 0; i < v1.size(); ++i)
+        cout << v[i] << " ";
+    cout << endl;
+
+    vector<int>::iterator iter_b = v1.begin();
+    vector<int>::iterator iter_e = v1.begin() + 2;
+    // 구간 [iter_b, iter_e) 의 순차열은 10, 20 이므로
+    // 찾는 원소가 없어 iter는 구간의 끝인 iter_e입니다.
+    iter = adjacent_find(iter_b, iter_e);
+
+
+
     return 0;
 }
