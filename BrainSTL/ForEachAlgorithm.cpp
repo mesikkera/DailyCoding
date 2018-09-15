fb17aeffff285e6a77951d42ba769e934ebba52a
mesikkera <mesikkera@gmail.com>
2018-09-15 12:25:42 +0900

    for_each() 알고리즘과 함수자

--- BrainSTL/ForEachAlgorithm.cpp
+++ BrainSTL/ForEachAlgorithm.cpp
@@ -8,6 +8,18 @@ void Print(int n)
     cout << n << " ";
 }
 
+class PrintFunctor
+{
+    char fmt;
+
+public:
+    explicit PrintFunctor(char c = ' ') : fmt(c) { }
+    void operator ()(int n) const 
+    {
+        cout << n << fmt;
+    }
+};
+
 int main()
 {
     vector<int> v;
@@ -24,6 +36,14 @@ int main()
 
     // [v.begin(), v.end()) 구간의 원소 출력
     for_each(v.begin(), v.end(), Print);
+    cout << endl;
+
+// for_each() 알고리즘과 함수자
+    for_each(v.begin(), v.end(), PrintFunctor());       // 원소 구분을 ' '로 
+    cout << endl;
+    for_each(v.begin(), v.end(), PrintFunctor(','));    // 원소 구분을 ','로
+    cout << endl;
+    for_each(v.begin(), v.end(), PrintFunctor('\n'));   // 원소 구분을 '\n'로
     cout << endl;
 
     return 0;
