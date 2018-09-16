966898a886ac0dcd66e066d0ea8e1fd29b8547d7
mesikkera <mesikkera@gmail.com>
2018-09-16 12:07:23 +0900

    조건자 버전의 lexicographical_compare() 알고리즘

--- BrainSTL/LexicographicalCompare.cpp
+++ BrainSTL/LexicographicalCompare.cpp
@@ -3,6 +3,14 @@
 #include <algorithm>
 using namespace std;
 
+template<typename T>
+struct Less 
+{
+    bool operator()(const T& left, const T& right) const 
+    {
+        return left < right;
+    }
+};
 int main()
 {
     vector<int> v1;
@@ -19,7 +27,7 @@ int main()
     v3.push_back(10);
     v3.push_back(20);
     v3.push_back(30);
-
+    
     if(lexicographical_compare(v1.begin(), v1.end(), v2.begin(), v2.end()))
         cout << "v1 < v2" << endl;
     else 
@@ -29,6 +37,33 @@ int main()
         cout << "v1 < v3" << endl;
     else 
         cout << "v1 >= v3" << endl;
+
+// 조건자 버전의 lexicographical_compare() 알고리즘
+    vector<int> v4;
+    v4.push_back(10);
+    v4.push_back(20);
+    v4.push_back(30);
+
+    vector<int> v5;
+    v5.push_back(10);
+    v5.push_back(25);
+    v5.push_back(50);
+
+    if(lexicographical_compare(v4.begin(), v4.end(), v5.begin(), v5.end(), less<int>()))
+        cout << "기준 less v4와 v5의 비교 : true" << endl;
+    else 
+        cout << "기준 less v4와 v5의 비교 : false" << endl;
+
+    if(lexicographical_compare(v4.begin(), v4.end(), v5.begin(), v5.end(), greater<int>()))
+        cout << "기준 greater v4와 v5의 비교 : true" << endl;
+    else 
+        cout << "기준 greater v4와 v5의 비교 : false" << endl;
+
+    if(lexicographical_compare(v4.begin(), v4.end(), v5.begin(), v5.end(), Less<int>()))
+        cout << "사용자 기준 less v4와 v5의 비교 : true" << endl;
+    else 
+        cout << "사용자 기준 less v4와 v5의 비교 : false" << endl;
+
 
     return 0;
 }
