38d9d1457076d0b4a2dbaf4f8e56d72dcab5e9f7
mesikkera <mesikkera@gmail.com>
2018-09-19 12:32:34 +0900

    remove_if() 알고리즘

--- BrainSTL/RemoveAlgorithm.cpp
+++ BrainSTL/RemoveAlgorithm.cpp
@@ -3,6 +3,11 @@
 #include <algorithm>
 using namespace std;
 
+int Pred(int n)
+{
+    return 30 <= n && n <= 40;
+}
+
 int main()
 {
     vector<int> v;
@@ -56,6 +61,26 @@ int main()
     cout << "v1 : ";
     for(vector<int>::size_type i = 0; i < v1.size(); ++i)
         cout << v1[i] << " ";
+    cout << endl;
+
+// remove_if() 알고리즘
+    vector<int> v2;
+    v2.push_back(10);
+    v2.push_back(20);
+    v2.push_back(30);
+    v2.push_back(40);
+    v2.push_back(50);
+
+    cout << "v2 : ";
+    for(vector<int>::size_type i = 0; i < v2.size(); ++i)
+        cout << v2[i] << " ";
+    cout << endl;
+
+    iter_end = remove_if(v2.begin(), v2.end(), Pred);
+
+    cout << "[v.begin(), iter_end) : ";
+    for(vector<int>::iterator iter = v2.begin(); iter != iter_end; ++iter)
+        cout << *iter << " ";
     cout << endl;
 
     return 0;
