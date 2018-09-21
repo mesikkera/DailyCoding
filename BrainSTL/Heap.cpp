3df679f674cbe7e65a30bd001371794636f6e862
mesikkera <mesikkera@gmail.com>
2018-09-21 15:00:58 +0900

    조건자 버전 힙 알고리즘

--- BrainSTL/Heap.cpp
+++ BrainSTL/Heap.cpp
@@ -25,5 +25,36 @@ int main()
         cout << v[i] << " ";
     cout << endl;
 
+// 조건자 버전 힙 알고리즘
+    vector<int> v1;
+    v1.push_back(40);
+    v1.push_back(10);
+    v1.push_back(50);
+    v1.push_back(30);
+    v1.push_back(20);
+    v1.push_back(60);
+
+    // 부모 노드가 모든 자식 노드보다 작은 힙을 생성
+    make_heap(v.begin(), v.end(), greater<int>());
+    cout << "v[힙 생성] : ";
+    for(vector<int>::size_type i = 0; i < v.size(); ++i)
+        cout << v[i] << " ";
+    cout << endl;
+
+    v.push_back(35);
+    push_heap(v.begin(), v.end(), greater<int>());
+    cout << "v[힙 추가] : ";
+    for(vector<int>::size_type i = 0; i < v.size(); ++i)
+        cout << v[i] << " ";
+    cout << endl;
+
+    sort_heap(v.begin(), v.end(), greater<int>());
+    cout << "v[힙 정렬] : ";
+    for(vector<int>::size_type i = 0; i < v.size(); ++i)
+        cout << v[i] << " ";
+    cout << endl;    
+
+
+
     return 0;
 }
