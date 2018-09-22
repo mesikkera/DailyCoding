61fd3ea1a9707c27f17b59d7bfe3e6637c555af4
mesikkera <mesikkera@gmail.com>
2018-09-22 12:08:54 +0900

    stable_sort() 알고리즘

--- BrainSTL/SortAlgorithm.cpp
+++ BrainSTL/SortAlgorithm.cpp
@@ -32,5 +32,33 @@ int main()
         cout << v[i] << " ";
     cout << endl;
 
+// stable_sort() 알고리즘
+    vector<int> v1;
+
+    v1.push_back(30);
+    v1.push_back(50);
+    v1.push_back(30);
+    v1.push_back(20);
+    v1.push_back(40);
+    v1.push_back(10);
+    v1.push_back(40);
+
+    cout << "v1[정렬 전] : ";
+    for(vector<int>::size_type i = 0; i < v1.size(); ++i)
+        cout << v1[i] << " ";
+    cout << endl;
+
+    stable_sort(v1.begin(), v1.end());
+    cout << "v1[정렬 후] : ";
+    for(vector<int>::size_type i = 0; i < v1.size(); ++i)
+        cout << v1[i] << " ";
+    cout << endl;
+
+    stable_sort(v1.begin(), v1.end(), Greater);
+    cout << "v1[정렬 greater] : ";
+    for(vector<int>::size_type i = 0; i < v1.size(); ++i)
+        cout << v1[i] << " ";
+    cout << endl;
+
     return 0;
 }
