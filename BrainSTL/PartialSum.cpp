96d151b96770a6f393b36ddbf4990e1322acd875
mesikkera <mesikkera@gmail.com>
2018-09-24 17:15:44 +0900

    partial_sum() 알고리즘

--- BrainSTL/PartialSum.cpp
+++ BrainSTL/PartialSum.cpp
@@ -3,6 +3,11 @@
 #include <numeric>
 using namespace std;
 
+int Multi(int left, int right)
+{
+    return left * right;
+}
+
 int main()
 {
     vector<int> v;
@@ -24,6 +29,15 @@ int main()
     cout << "v2: ";
     for(vector<int>::size_type i = 0; i < v2.size(); ++i)
         cout << v2[i] << " ";
+    cout << endl;
+
+// 함수류 버전 partial_sum 알고리즘
+    vector<int> v3(5);
+    iter_end = partial_sum(v.begin(), v.end(), v3.begin(), Multi);
+
+    cout << "v3: ";
+    for(vector<int>::size_type i = 0; i < v3.size(); ++i)
+        cout << v3[i] << " ";
     cout << endl;
 
     return 0;
