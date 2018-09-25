0c6551aa49e83114b0fdd352cf1f68f9194bc91c
mesikkera <mesikkera@gmail.com>
2018-09-25 15:00:14 +0900

    r-value 참조

---
+++ BrainCpp/RvalueSample.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int TestFunc(int nParam)
+{
+    int nResult = nParam * 2;
+
+    return nResult;
+}
+
+int main()
+{
+    int nInput = 0;
+    cout << "Input Number: ";
+    cin >> nInput;
+
+    // 산술 연산으로 만들어진 임시 객체에 대한 r-value 참조
+    int &&rdata = nInput + 5;
+    cout << rdata << endl;
+
+    // 함수 반환으로 만들어진 임시 객체에 대한 r-value 참조
+    int &&result = TestFunc(10);
+
+    // 값을 변경할 수 있다.
+    result += 10;
+    cout << result << endl;
+
+    return 0;
+}
