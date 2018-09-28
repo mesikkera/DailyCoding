883d334b1528d35c5239beb5e319be2c6cee04a7
mesikkera <mesikkera@gmail.com>
2018-09-28 14:49:30 +0900

    STL 함수 객체

--- BrainSTL/STLFunctionObject/STLFunctionObject/main.cpp
+++ BrainSTL/STLFunctionObject/STLFunctionObject/main.cpp
@@ -7,9 +7,12 @@
 //
 
 #include <iostream>
+#include <functional>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main() {
+    binder2nd<less<int>> binder = bind2nd(less<int>(), 10);
+    
+    cout << binder(5) << ":" << less<int>()(5, 10) << endl;
     return 0;
 }
