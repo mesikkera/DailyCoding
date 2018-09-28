4495534bc055648396b44fccff549d7d8efb0872
mesikkera <mesikkera@gmail.com>
2018-09-28 16:52:16 +0900

    더하기

---
+++ BaekJoon/14918.cpp
@@ -0,0 +1,19 @@
+/*
+C++ 입출력 예제
+
+#include <iostream>
+…
+cin >> a >> b;
+cout << a+b << endl;
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a(0), b(0);
+    cin >> a >> b;
+    cout << a + b << '\n';
+    return 0;
+}
