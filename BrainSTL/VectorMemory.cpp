0fd1b227ef50c033b8ed13ddeeb5ff99113821bd
mesikkera <mesikkera@gmail.com>
2018-10-05 18:10:24 +0900

    vector의 메모리 관리 정책1

---
+++ BrainSTL/VectorMemory.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    vector<int> v;
+
+    v.push_back(10);
+    v.push_back(20);
+
+    cout << "v[0] : " << v[0] << endl;
+    cout << "v.at(0) : " << v.at(0) << endl;
+    cout << "v[1] : " << v[1] << endl;
+    cout << "v.at(1) : " << v.at(1) << endl;
+    cout << "v[2] : " << v[2] << endl;
+    // cout << "v.at(2) : " << v.at(2) << endl;
+    return 0;
+}
