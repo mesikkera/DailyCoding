c196d14fd9f33a7790713f1e9125c138dca3510b
mesikkera <mesikkera@gmail.com>
2018-10-06 16:21:09 +0900

    네임스페이스 직접 정의하기

--- BrainCpp/Namespace.cpp
+++ BrainCpp/Namespace.cpp
@@ -1,8 +1,22 @@
-#include "cat.h"
+#include <iostream>
+using namespace std;
+
+// first 네임스페이스 정의
+namespace first 
+{
+    int value = 1;
+}
+
+// second 네임스페이스 정의
+namespace second 
+{
+    int value = 2;
+}
 
 int main()
 {
-    Cat::CreateAll();
-
+    int i = 0;
+    cout << first::value << endl;
+    cout << second::value << endl;
     return 0;
 }
