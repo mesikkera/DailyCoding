1706c6a410a0bae24e6296a9decd77fdc70152e3
mesikkera <mesikkera@gmail.com>
2018-10-09 15:16:11 +0900

    참조자의 이해

--- BrainCpp/reference1.cpp
+++ BrainCpp/reference1.cpp
@@ -5,6 +5,8 @@ int main()
 {
     using salary = double;
     salary peter = 100.12;
+    // 참조자 선언은 특정 변수의 별칭을 선언하는 것이므로
+    // 반드시 특정 변수로 초기화를 해줘야 한다.
     salary &sangwon = peter;
     cout << "Peter's memory address = " << &peter << endl;
     cout << "sangwon's memory address = " << &sangwon << endl;
