0a5d952cbadfce47e02bae36e079ec7bb86dca40
mesikkera <mesikkera@gmail.com>
2018-10-10 15:03:53 +0900

    변수에 값을 복사

---
+++ BrainCpp/CallByValue.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+    int value = 3;
+    int i = value;
+    i = 33;
+
+    cout << "value = " << value << endl;
+    cout << "i = " << i << endl;
+
+    return 0;
+}
