dd270b999ddbbda12cad3a7bc64dd23a50cf2b1a
mesikkera <mesikkera@gmail.com>
2018-10-11 15:52:23 +0900

    람다 함수의 반환타입

--- BrainCpp/Lambda2.cpp
+++ BrainCpp/Lambda2.cpp
@@ -26,5 +26,21 @@ int main(void)
         // 람다함수 내부에서 외부 변수를 수정할 수 있게 하려면 [&] 캡처를 사용해야 한다.
         [&](int steakWeight){ cout << "eatLambda()::철수는 " << chulsoo.count++ << "번 째" << steakWeight << "g짜리 스테이크를 먹는다." << endl;} (1000);
     }
+
+    // 반환형을 char로 지정한 람다 함수
+    cout << [=](int steakWeight) mutable -> char { 
+        cout << "eatLambda()::철수는 " << chulsoo.count++ << "번째 " << steakWeight << "g짜리 스테이크를 먹는다." << endl; 
+        return steakWeight;} (67) << endl;
+
+    // 반환형을 int로 지정한 람다 함수
+    cout << [=](int steakWeight) mutable->int { 
+        cout << "eatLambda()::철수는 " << chulsoo.count++ << "번째 " << steakWeight << "g짜리 스테이크를 먹는다." << endl;
+        return steakWeight; } (67) << endl;
+
+    // 반환형을 지정하지 않았지만 steakWeight를 반환하므로 int
+    cout << [=](int steakWeight) mutable { 
+        cout << "eatLambda()::철수는 " << chulsoo.count++ << "번째 " << steakWeight << "g짜리 스테이크를 먹는다." << endl;
+        return steakWeight; } (67) << endl;
+        
     return 0;
 }
