e90920c93a2246f630da3580e27d662e2b2e4c7f
mesikkera <mesikkera@gmail.com>
2018-10-14 11:11:32 +0900

    클래스 멤버 함수 선언 및 정의 분리

--- BrainCpp/classMemberFunctionDefinition.cpp
+++ BrainCpp/classMemberFunctionDefinition.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 #define NAME_LEN 20
@@ -43,3 +44,105 @@ public:
     void shopping();
     void constructor(char* name, char* sex, char* job, char* character, int age, bool marriageStatus);
 };
+
+int main(void)
+{
+    Chulsoo chulsoo;
+    Younghee younghee;
+    chulsoo.constructor("철수", "남성", "작가", "diligent", 32, true);
+    younghee.constructor("영희", "여성", "주부", "impatient", 32, true);
+
+    chulsoo.drive("레스토랑");
+    chulsoo.eat("스테이크");
+    younghee.eat("스테이크");
+    chulsoo.drive("집");
+    younghee.sleep();
+    chulsoo.write();
+    chulsoo.read();
+    chulsoo.sleep();
+    cout << endl;
+    chulsoo.introduce();
+    cout << endl;
+    younghee.introduce();
+
+    return 0;
+}
+
+void Chulsoo::introduce()
+{
+    cout << "이름: " << name << endl;
+    cout << "성별: " << sex << endl;
+    cout << "직업: " << job << endl;
+    cout << "성격: " << character << endl;
+    cout << "나이: " << age << endl;
+    cout << "결혼여부: " << (marriageStatus ? "YES" : "NO") << endl;
+}
+
+void Chulsoo::eat(char* food)
+{
+    cout << "철수는 " << food << "를 먹는다." << endl;
+}
+
+void Chulsoo::sleep()
+{
+    cout << "철수는 잔다." << endl;
+}
+
+void Chulsoo::drive(char* destination)
+{
+    cout << "철수는 " << destination << "으로 운전한다." << endl;
+}
+
+void Chulsoo::write()
+{
+    cout << "철수는 책을 쓴다." << endl;
+}
+
+void Chulsoo::read()
+{
+    cout << "철수는 책을 읽는다." << endl;
+}
+
+void Chulsoo::constructor(char* name, char* sex, char* job, char* character, int age, bool marriageStatus)
+{
+    strcpy(this->name, name);
+    strcpy(this->sex, sex);
+    strcpy(this->job, job);
+    strcpy(this->character, character);
+    this->age = age;
+    this->marriageStatus = marriageStatus;
+}
+
+void Younghee::introduce()
+{
+    cout << "이름: " << name << endl;
+    cout << "성별: " << sex << endl;
+    cout << "직업: " << job << endl;
+    cout << "나이: " << age << endl;
+    cout << "결혼여부: " << (marriageStatus ? "YES" : "NO") << endl;
+}
+
+void Younghee::eat(char* food)
+{
+    cout << "영희는 " << food << "를 먹는다." << endl;
+}
+
+void Younghee::sleep()
+{
+    cout << "영희는 잔다." << endl;
+}
+
+void Younghee::shopping()
+{
+    cout << "영희는 쇼핑을 한다." << endl;
+}
+
+void Younghee::constructor(char* name, char* sex, char* job, char* character, int age, bool marriageStatus)
+{
+    strcpy(this->name, name);
+    strcpy(this->sex, sex);
+    strcpy(this->job, job);
+    strcpy(this->character, character);
+    this->age = age;
+    this->marriageStatus = marriageStatus;
+}
