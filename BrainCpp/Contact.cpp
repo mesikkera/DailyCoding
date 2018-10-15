8c915de35722b07985a138abc3823142362f9d13
mesikkera <mesikkera@gmail.com>
2018-10-15 17:13:14 +0900

    소유자 클래스 구현

--- BrainCpp/Contact.cpp
+++ BrainCpp/Contact.cpp
@@ -11,6 +11,24 @@ struct OwnerInfo {
     char twitterAccount[20];    // 트위터 계정
 };
 
+class Owner 
+{
+private:
+    char name[20];              // 이름
+    char phoneNumber[13];       // 전화번호
+    char email[30];             // 이메일
+    char address[50];           // 주소
+    char twitterAccount[20];    // 트위터 계정
+
+public:
+    // 소유자 정보 입력
+    void inputOwnerInfo(char* inName, char* inPhoneNumber=NULL, char* inEmail=NULL, char* inAddress=NULL, char* inTwitterAccount=NULL);
+    // 소유자 정보 출력
+    void printOwnerInfo();
+    // 소유자 정보 수정
+    void editOwnerInfo(char* inName, char* inPhoneNumber=NULL, char* inEmail=NULL, char* inAddress=NULL, char* inTwitterAccount=NULL);
+};
+
  // 연락처 정보
  struct ContactInfo {
      char name[20];             // 이름
