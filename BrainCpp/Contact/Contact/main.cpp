119ee0b915e17752c127a77ba9d6554ec78c6482
mesikkera <mesikkera@gmail.com>
2018-10-15 21:06:24 +0900

    Contract 프로그램 Xcode 프로젝트로 변경

---
+++ BrainCpp/Contact/Contact/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Contact
+//
+//  Created by mesikkera on 15/10/2018.
+//  Copyright © 2018 mesikkera. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
