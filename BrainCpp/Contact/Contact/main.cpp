05eb9d4601e0079913a8433d1bbb376343c1d87b
mesikkera <mesikkera@gmail.com>
2018-10-16 19:38:33 +0900

    Contact 프로그램 완성

--- BrainCpp/Contact/Contact/main.cpp
+++ BrainCpp/Contact/Contact/main.cpp
@@ -6,10 +6,10 @@
 //  Copyright © 2018 mesikkera. All rights reserved.
 //
 
-#include <iostream>
+#include "Controller.h"
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main() {
+    Controller control;
+    control.runMainMenu();
     return 0;
 }
