2f849cb71118d47fb246ba644292aaf23331c7cd
mesikkera <mesikkera@gmail.com>
2018-10-19 20:22:32 +0900

    대입 연산자 오버로딩

---
+++ BrainCpp/assignOperatorOverloading1.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std;
+
+class Chulsoo 
+{
+private:
+    int age;
+
+public:
+    Chulsoo(const Chulsoo & source) : age(source.age)
+    {
+        cout << "Chulsoo::Chulsoo() 복사 생성자 완료" << endl;
+    }
+
+    Chulsoo(int age) : age(age) 
+    {
+        cout << "Chulsoo::Chulsoo(age) 생성자 완료" << endl;
+    }
+
+    Chulsoo()
+    {
+        cout << "Chulsoo::Chulsll() 생성자 완료" << endl;
+    }
+
+    void introduce();
+
+    ~Chulsoo()
+    {
+        cout << "Chulsoo::~Chulsoo() 소멸자 완료" << endl;
+    }
+};
+
+int main(void)
+{
+    Chulsoo chulsoo1(32);
+    Chulsoo chulsoo2(50);
+
+    cout << "===============대입 연산 전===============" << endl;
+    chulsoo1.introduce();
+    chulsoo2.introduce();
+    
+    cout << "===============대입 연산 후===============" << endl;
+    chulsoo1 = chulsoo2;
+    chulsoo1.introduce();
+    chulsoo2.introduce();
+    return 0;    
+}
+
+void Chulsoo::introduce()
+{
+    cout << "age : " << age << endl;
+}
