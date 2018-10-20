b277c68dc06e8f90cb4eb2f8fcf89ba69681f6ae
mesikkera <mesikkera@gmail.com>
2018-10-20 21:16:03 +0900

    스마트 포인터 auto_ptr 사용

---
+++ BrainCpp/smartPointer1.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <memory>
+using namespace std;
+
+class Chulsoo 
+{
+private:
+    int age;
+public:
+    Chulsoo(int age) : age(age)
+    {
+        cout << "Chulsoo::Chulsoo(age) 생성자 완료" << endl;
+    }
+    Chulsoo()
+    {
+        cout << "Chulsoo::Chulsoo() 생성자 완료" << endl;
+    }
+    ~Chulsoo()
+    {
+        cout << "Chulsoo::~Chulsoo() 소멸자 완료" << endl;
+    }
+    void setAge(int age)
+    {
+        this->age = age;
+    }
+    void introduce();
+};
+
+int main(void)
+{
+    // Chulsoo* chulsooPtr2 = new Chulsoo(32);
+    // chulsooPtr2->introduce();
+    // delete chulsooPtr2;
+
+    auto_ptr<Chulsoo> chulsooSmptr(new Chulsoo(32));
+    chulsooSmptr->introduce();
+    return 0;
+}
+
+void Chulsoo::introduce()
+{
+    cout << "철수 나이 : " << age << endl;
+}
