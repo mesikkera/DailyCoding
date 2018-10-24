02645975df77b71be20a9e75e912b380d3557473
mesikkera <mesikkera@gmail.com>
2018-10-24 16:57:14 +0900

    함수 오버라이딩

---
+++ BrainCpp/functionOverriding.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+using namespace std;
+
+#define NAME_LEN 20
+
+class Person 
+{
+protected:
+    char name[NAME_LEN];
+    int age;
+public:
+    Person(char* name, int age) 
+    {
+        strlcpy(this->name, name, NAME_LEN);
+        this->age = age;
+        cout << "Person::Person(name, age) 생성자 완료" << endl;
+    }
+    ~Person()
+    {
+        cout << "Person::~Person() 소멸자 완료" << endl;
+    }
+    void introduce()
+    {
+        cout << "Person의 이름 : " << name << endl;
+        cout << "Person의 나이 : " << age << endl;
+    }
+    void eat()
+    {
+        cout << "Person은 먹는다." << endl;
+    }
+    void sleep()
+    {
+        cout << "Person은 잔다." << endl;
+    }
+};
+
+class Chulsoo : public Person 
+{
+private:
+    int numOfBooks;
+public:
+    Chulsoo(char* name, int age, int numOfBooks) : Person(name, age) 
+    {
+        this->numOfBooks = numOfBooks;
+        cout << "Chulsoo::Chulsoo(name, age, numOfBooks) 생성자 완료" << endl;
+    }
+    ~Chulsoo()
+    {
+        cout << "Chulsoo::~Chulsoo() 소멸자 완료" << endl;
+    }
+};
+
+int main()
+{
+    Person person("사람", 100);
+    person.introduce();
+
+    Chulsoo chulsoo("철수", 32, 3);
+    chulsoo.introduce();
+    chulsoo.eat();
+    chulsoo.sleep();
+
+    return 0;
+}
