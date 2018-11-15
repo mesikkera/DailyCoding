e69445bb16562368c8de5d9c3183368af94966f4
mesikkera <mesikkera@gmail.com>
2018-11-15 16:50:59 +0900

    프로세스 기다리기

--- LinuxC/wait.c
+++ LinuxC/wait.c
@@ -35,4 +35,18 @@ int main()
 		puts(message);
 		sleep(1);
 	}
+
+	if(pid != 0) {
+		int stat_val;
+		pid_t child_pid;
+
+		child_pid = wait(&stat_val);
+
+		printf("Child has finished: PID = %d\n", child_pid);
+		if(WIFEXITED(stat_val))
+			printf("Child exited with code %d\n", WEXITSTATUS(stat_val));
+		else
+			printf("Child terminated abnormally\n");
+	}
+	exit(exit_code);
 }
