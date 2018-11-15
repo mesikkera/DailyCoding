0bd1c538a1ba7839df95ef98d04d5d87cdb08ff0
mesikkera <mesikkera@gmail.com>
2018-11-15 16:35:29 +0900

    main 함수 수정

--- ToyProject/GradeCard/GradeCard/main.cpp
+++ ToyProject/GradeCard/GradeCard/main.cpp
@@ -7,9 +7,29 @@
 //
 
 #include <iostream>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main() {
+    
+    // 계속해서 메뉴를 보여준다.
+    while(1) {
+        // 메뉴 보여주기
+        cout << "\n----- 메뉴 -----\n";
+        cout << "1. 학생 성적 추가\n";
+        cout << "2. 전체 성적 보기\n";
+        cout << "-------------------";
+        cout << "원하는 작업의 번호를 입력하세요 : ";
+        
+        // 작업 입력 받기
+        char select;
+        cin >> select;
+        
+        // 선택된 값에 따라서 처리
+        switch(select) {
+            case '1':
+                // 학생 성적 추가
+                cout << "\n학생 성적 추가가 선택되었습니다.";
+        }
+    }
     return 0;
 }
