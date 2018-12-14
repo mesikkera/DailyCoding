c7d7f5fd85d3ddb5cce1ae22fd0f5a27ef1c5c95
mesikkera <mesikkera@gmail.com>
2018-12-14 16:35:52 +0900

    Sync Timer

---
+++ Boost/sync_timer.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <boost/asio.hpp>
+#include <boost/date_time/posix_time/posix_time.hpp>
+
+int main() {
+	boost::asio::io_service io;
+
+	for(int i = 0; i < 5; i++) {
+		boost::asio::deadline_timer(io, boost::posix_time::seconds(i));
+		timer.wait();
+		std::cout << "Blocking wait(): " << i << " second-wait\n";
+	}
+
+	return 0;
+}
