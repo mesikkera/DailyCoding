344157ce30f995407f8db246307941642680dd93
mesikkera <mesikkera@gmail.com>
2019-01-05 20:35:00 +0900

    단항 연산자 오버로딩

--- BrainCpp/OperatorOverloadingExample.cpp
+++ BrainCpp/OperatorOverloadingExample.cpp
@@ -35,17 +35,42 @@ public:
 
 		return pt;
 	}
+
+	const Point& operator++()		// 전위 ++ 연산자 
+	{
+		++x;
+		++y;
+		return *this;
+
+	}
+
+	const Point& operator++(int)	// 후위 ++ 연산자
+	{
+		x++;
+		y++;
+		return *this;
+	}
 };
 
 int main()
 {
 	Point p1(2, 3), p2(5, 5);
-	Point p3;
-	Point p4;
+	Point p3, p4;
+	Point result;
 
 	p3 = p1 + p2;
 	p3.Print(); 
 	p4 = p1 - p2;		// p1.operator-(p2); 와 동일.
 	p4.Print();
 
+	++p1;				// p1.operator++(); 와 동일.
+	p2++;				// p2.operator++(int); 와 동일.
+
+	result = ++p1;		// p1.operator++();와 동일.
+	p1.Print();
+	result.Print();
+
+	result = p2++;
+	p2.Print();
+	result.Print();
 }
