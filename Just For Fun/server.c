7d9cf86ab3714b9a3de63459af68b0af37229750
mesikkera <mesikkera@gmail.com>
2019-01-28 14:38:23 +0900

    server example

---
+++ Just For Fun/server.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+void error(const char *msg)
+{
+    perror(msg);
+    exit(1);
+}
+
+int main(int argc, char *argv[])
+{
+    int sockfd, newsockfd, portno;
+    sockelen_t clien;
+}
