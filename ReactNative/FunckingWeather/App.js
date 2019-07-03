44e5510808c0e7f87e62da50a2d3f7d67bb7c484
mesikkera <mesikkera@gmail.com>
2019-07-03 16:50:19 +0900

    Fucking Weather App.js Default Settings

--- ReactNative/FunckingWeather/App.js
+++ ReactNative/FunckingWeather/App.js
@@ -1,19 +1,37 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, Image, ActivityIndicator } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+export default class App extends Component {
+  state = {
+    isLoaded: false 
+  }
+  render() {
+    const { isLoaded } = this.state;
+    return (
+      <View style={styles.container}>
+        {isLoaded ? null : (
+        <View style={styles.loading}>
+          <Text style={styles.loadingText}>FeeLWAY App 2.0</Text>
+        </View>
+        )}
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  loading: { 
+    flex: 1, 
+    backgroundColor: "#FDF6AA",
+    justifyContent: "flex-end",
+    paddingLeft: 25,
+  },
+  loadingText: {
+    fontSize: 38,
+    marginBottom: 100
+  }
 });
---
+++ ReactNative/FunckingWeather/Weather.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { LinearGradient } from 'expo';
+
+export default class Weather extends Component {
+    render() {
+   
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    }
+})
