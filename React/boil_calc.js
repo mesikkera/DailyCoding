218b13c1ab1e311bd6d6fb28d01f27e779d161a1
mesikkera <mesikkera@gmail.com>
2019-07-17 18:49:18 +0900

    state 끌어올리기

---
+++ React/boil_calc.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+function BoilingVerdict(props) {
+    if (props.celsius >= 100) {
+        return <p>The water would boil.</p>
+    }
+    return <p>The water would not boil.</p>
+}
+
+class Calculator extends React.Component {
+
+}
--- React/research.js
+++ React/research.js
@@ -152,3 +152,13 @@ class EssayForm extends React.Component {
         }
     }
 }
+
+// State 끌어올리기
+
+function BoilingVerdict(props) {
+    if (props.celsius >= 100) {
+        return <p>The water would boil.</p>
+    }
+    return <p>The water would not boil.</p>
+}
+
