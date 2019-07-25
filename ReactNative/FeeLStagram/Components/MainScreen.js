74388f77e7a1e676750621e37a5a2d7f66239abe
mesikkera <mesikkera@gmail.com>
2019-07-25 14:40:35 +0900

    상단 네비게이션 구현

--- ReactNative/FeeLStagram/Components/MainScreen.js
+++ ReactNative/FeeLStagram/Components/MainScreen.js
@@ -1,7 +1,15 @@
-import React, { Component } from 'react';
-import { StyleSheet, Platform, View, Text } from 'react-native';
+import React, { Component } from "react";
+import { StyleSheet, Platform, View, Text } from "react-native";
+import { Icon } from "native-base";
 
 export default class MainScreen extends Component {
+  //navigationOPtions 코드 추가
+  static navigationOptions = {
+    headerLeft: <Icon name="ios-camera" style={{ paddingLeft: 10 }} />,
+    title: "FeeLstagram",
+    headerRight: <Icon name="ios-send" style={{ paddingRight: 10 }} />
+  };
+
   render() {
     return (
       <View style={styles.container}>
@@ -14,7 +22,7 @@ export default class MainScreen extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
