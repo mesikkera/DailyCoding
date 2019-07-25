15c5218960950057b918a6c1d7f3ec96097acaa8
mesikkera <mesikkera@gmail.com>
2019-07-25 11:25:46 +0900

    상단 네비게이션 구현

--- ReactNative/CloneInstagram/Components/MainScreen.js
+++ ReactNative/CloneInstagram/Components/MainScreen.js
@@ -1,7 +1,15 @@
 import React, { Component } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import { Icon } from 'native-base';
 
 export default class MainScreen extends Component {
+  // navigationOPtion 코드 추가
+  static navigationOptions = {
+    headerLeft: <Icon name="ios-camera" style={{ paddingLeft: 10 }} />,
+    title: 'FeeLStagram',
+    headerRigith: <Icon name="ios-send" style={{ paddingRight: 10 }} />,
+  };
+
   render() {
     return (
       <View style={styles.container}>
