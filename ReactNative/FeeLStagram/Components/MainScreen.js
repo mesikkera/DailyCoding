41cfc4098aeac1b2904f75b8ebc4104c6b1113b4
mesikkera <mesikkera@gmail.com>
2019-07-25 15:55:15 +0900

    텝 네비게이터에 스와이프와 애니매이션 넣기

--- ReactNative/FeeLStagram/Components/MainScreen.js
+++ ReactNative/FeeLStagram/Components/MainScreen.js
@@ -1,7 +1,10 @@
 import React, { Component } from "react";
 import { StyleSheet, Platform, View, Text } from "react-native";
 import { Icon } from "native-base";
-import { createBottomTabNavigator, createAppContainer } from "react-navigation";
+import {
+  createMaterialTopTabNavigator,
+  createAppContainer
+} from "react-navigation";
 
 // 하단 탭에 들어갈 컴포넌트들
 import HomeTab from "./AppTabNavigator/HomeTab";
@@ -11,13 +14,35 @@ import LikesTab from "./AppTabNavigator/LikesTab";
 import ProfileTab from "./AppTabNavigator/ProfileTab";
 
 // 하단 탭 네비게이터 생성
-const AppTabNavigator = createBottomTabNavigator({
-  HomeTab: { screen: HomeTab },
-  SearchTab: { screen: SearchTab },
-  AddMediaTab: { screen: AddMediaTab },
-  LikesTab: { screen: LikesTab },
-  ProfileTab: { screen: ProfileTab }
-});
+const AppTabNavigator = createMaterialTopTabNavigator(
+  {
+    HomeTab: { screen: HomeTab },
+    Search: { screen: SearchTab },
+    AddMedia: { screen: AddMediaTab },
+    Likes: { screen: LikesTab },
+    Profile: { screen: ProfileTab }
+  },
+  {
+    animationEnabled: true,
+    swipeEnabled: true,
+    tabBarPosition: "bottom",
+    tabBarOptions: {
+      style: {
+        ...Platform.select({
+          ios: {
+            backgroundColor: "white"
+          }
+        })
+      },
+      iconStyle: { height: 100 },
+      activeTintColor: "#000",
+      inactiveTintColor: "#d1cece",
+      upperCaseLabel: false,
+      showLabel: false,
+      showIcon: true
+    }
+  }
+);
 
 const AppTabContainet = createAppContainer(AppTabNavigator);
 
