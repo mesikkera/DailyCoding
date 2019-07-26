58c5d3ecfd5643bc50b4ecdf8437664bccd3a23a
mesikkera <mesikkera@gmail.com>
2019-07-26 11:41:47 +0900

    SerachTab 아이콘 미노출 해결

--- ReactNative/FeeLStagram/Components/AppTabNavigator/SearchTab.js
+++ ReactNative/FeeLStagram/Components/AppTabNavigator/SearchTab.js
@@ -1,17 +1,17 @@
-import React, { Component } from "react";
-import { View, Text, StyleSheet } from "react-native";
-import { Icon } from "native-base";
+import React, { Component } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { Icon } from 'native-base';
 
-export default class SearchTab extends Component {
-  static navigationOPtions = {
+export default class ProfileTab extends Component {
+  static navigationOptions = {
     tabBarIcon: ({ tintColor }) => (
       <Icon name="ios-search" style={{ color: tintColor }} />
-    )
+    ),
   };
   render() {
     return (
       <View style={style.container}>
-        <Text>SearchTab</Text>
+        <Text>ProfileTab</Text>
       </View>
     );
   }
@@ -20,7 +20,7 @@ export default class SearchTab extends Component {
 const style = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center"
-  }
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
 });
