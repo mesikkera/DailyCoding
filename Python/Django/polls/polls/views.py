26027d0b87abe6df1c9af45bf9b251cdc461a698
mesikkera <mesikkera@gmail.com>
2019-08-16 01:55:59 +0900

    implement simple view page and set urls

--- Python/Django/polls/polls/views.py
+++ Python/Django/polls/polls/views.py
@@ -5,3 +5,10 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+def detail(request, question_id):
+    return HttpResponse("You're lokking at question %s." % question_id)
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
