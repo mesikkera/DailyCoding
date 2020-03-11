51fa14b327f42d9e08d55cb1a5587c9c5bab7375
mesikkera <mesikkera@gmail.com>
2020-03-11 23:42:00 +0900

    BeautifulSoup usage

--- Crawling/naver_crawling.py
+++ Crawling/naver_crawling.py
@@ -1,6 +1,14 @@
 from urllib.request import urlopen
+import bs4
 
-url = "https://www.naver.com"
-html = urlopen(url)
+html_str = "<html><div>test</div></html>"
+bs_obj = bs4.BeautifulSoup(html_str, 'html.parser')
 
-print(html.read())
+print(type(bs_obj))
+print(bs_obj)
+print(bs_obj.find('div'))
+
+# url = "https://www.naver.com"
+# html = urlopen(url)
+
+# print(html.read())
