5be2bb644b27aca9d8bb0867b704cf6a9b721d76
mesikkera <mesikkera@gmail.com>
2024-10-12 22:43:28 +0900

    BaekJoon 2588

---
+++ BaekJoon/2588.py
@@ -0,0 +1,7 @@
+first_input = int(input())
+second_input = input()
+
+print(first_input * int(second_input[2]))
+print(first_input * int(second_input[1]))
+print(first_input * int(second_input[0]))
+print(first_input * int(second_input))
