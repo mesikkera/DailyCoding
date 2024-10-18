8356bffa14315eef696753c4ff70e9d000b1f37f
mesikkera <mesikkera@JiYoons-M1-Mac-Studio.local>
2024-10-18 16:04:22 +0900

    Modify pybo view page

--- Python/Django/mysite/pybo/views.py
+++ Python/Django/mysite/pybo/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-	return HttpResponse("안녕하세요 pybo에 오신것을 환영합니다.")
+	return HttpResponse("Hi, Welcome to Pybo!")
 
