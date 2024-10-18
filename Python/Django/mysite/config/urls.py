f81d9fabb61e502bc862cc04ee9d6f78ddfce853
mesikkera <mesikkera@gmail.com>
2024-10-18 16:15:12 +0900

    Splitting the urls.py file

--- Python/Django/mysite/config/urls.py
+++ Python/Django/mysite/config/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
-from pybo import views
+# from pybo import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-	path('pybo/', views.index),
+	# path('pybo/', views.index),
+    path('pybo/', include('pybo.urls')),
 ]
