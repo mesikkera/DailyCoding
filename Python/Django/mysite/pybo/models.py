a01215199973f77bea5f6b71e7287bc62ecc90ff
mesikkera <mesikkera@gmail.com>
2024-10-21 16:10:06 +0900

    Create Model

--- Python/Django/mysite/pybo/models.py
+++ Python/Django/mysite/pybo/models.py
@@ -2,7 +2,9 @@ from django.db import models
 
 
 class Question(models.Model):
+    # 글자수의 길이가 제한된 테스트 - CharField 사용
     subject = models.CharField(max_length=200)
+    # 글자수를 제한할 수 없는 텍스트 - TextField 사용
     content = models.TextField()
     create_date = models.DateTimeField()
 
